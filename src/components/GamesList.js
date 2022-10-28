f52ec92e7e0e40dafbafd2e9dbd33b719ad037ad
Brandon <bcharris713@gmail.com>
2022-10-28 17:21:10 -0500

    added comments, gonna try to add more

--- src/components/GamesList.js
+++ src/components/GamesList.js
@@ -2,15 +2,18 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function GamesList({ games }) {
+  /*creates variable 'render games'
+   *maps through games
+   *assigns a link to each game title
+   *sets a new page extension equal to the gameID*/
   const renderGames = Object.keys(games).map((gameID) => (
-    
     <div key={gameID}>
       <Link to={`/games/${gameID}`}>
           {games[gameID].title}   
       </Link>
     </div>
   ));
-
+  //returns render games wrapped in div element  
   return <div>{renderGames}</div>;
 }
 
