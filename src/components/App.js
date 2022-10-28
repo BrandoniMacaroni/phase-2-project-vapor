b0bec0d872877ae63754c3f26e8b26d06d7bdf05
Brandon <bcharris713@gmail.com>
2022-10-28 16:59:33 -0500

    added data fetching

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
 import {  Route, Switch } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 import React from "react";
 import NavBar from "./NavBar";
 import GamesPage from "./GamesPage";
 import GameForm from "./GameForm";
 
 function App() {
+  //connecting setGames and games
+  const [games, setGames] = useState([]);
+
+  //pulling data from json and using setGames()
+  useEffect(() => {
+    fetch("http://localhost:3001/games")
+      .then((r) => r.json())
+      .then(setGames);
+  }, []);
 
   return (
     <div>
