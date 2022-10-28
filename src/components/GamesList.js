b2322505e27b6eaa6d3a77ef243d7e960387e65f
Brandon <bcharris713@gmail.com>
2022-10-28 17:08:28 -0500

    printing the games using GamesList.js

--- src/components/GamesList.js
+++ src/components/GamesList.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-function GamesList() {
+function GamesList({ games }) {
+  const renderGames = Object.keys(games).map((gameID) => (
+    
+    <div key={gameID}>
+      <Link to={`/games/${gameID}`}>
+          {games[gameID].title}   
+      </Link>
+    </div>
+  ));
 
-  return (
-    <div>hi</div>
-  );
+  return <div>{renderGames}</div>;
 }
 
 export default GamesList;
