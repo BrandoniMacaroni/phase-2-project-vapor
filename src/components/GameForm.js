038386f5dc7762273e81bf3a0370ec03aeffaa76
Brandon <bcharris713@gmail.com>
2022-10-28 17:47:27 -0500

    added the react now gotta add the html

--- src/components/GameForm.js
+++ src/components/GameForm.js
@@ -1,15 +1,38 @@
 import React from "react";
+import { useState } from "react";
 
 function GameForm({ onAddGame }) {
-    fetch("http://localhost:3001/games", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(newGame),
-    })
-        .then((r) => r.json())
-        .then(onAddGame);
+    const [formData, setFormData] = useState({
+        title: "",
+        image: "",
+        style: "",
+        pace: "",
+        price: ""
+    });
+
+    function handleChange(event) {
+        setFormData({
+            ...formData,
+            [event.target.name]: event.target.value,
+        });
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        const newGame = {
+            ...formData,
+        };
+
+        fetch("http://localhost:3001/games", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newGame),
+        })
+            .then((r) => r.json())
+            .then(onAddGame);
+    }
 
     return (
         <div>hi</div>
