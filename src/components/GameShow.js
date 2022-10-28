00ff5b3e1ee4d6111500a35c9f4ff7bbc67803be
Brandon <bcharris713@gmail.com>
2022-10-28 17:26:16 -0500

    setup GameShow.js to print each game depending on routeID

--- src/components/GameShow.js
+++ src/components/GameShow.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
-function GameShow() {
+function GameShow({ games, onDeleteGame }) {
+  const params = useParams();
 
   return (
-    <div>hi</div>
+    <div>
+        <div><img src={games[params.gameId].image} alt={"name"}/></div>
+        <div>
+          <h1>{games[params.gameId].title}</h1>
+          <h3>{games[params.gameId].style}</h3>
+          <h3>{games[params.gameId].pace}</h3>
+          <h3>{games[params.gameId].price}</h3>
+        </div>
+    </div>
   );
 }
 
