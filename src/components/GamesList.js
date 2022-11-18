eaa81302ebeb9cb1c7aec76c1edb75310cfa4d98
Brandon <bcharris713@gmail.com>
2022-11-18 13:34:03 -0600

    removed object.keys and replaced with a more streamlined function

--- src/components/GamesList.js
+++ src/components/GamesList.js
@@ -3,15 +3,16 @@ import { Link } from "react-router-dom";
 import './App.css';
 
 function GamesList({ games }) {
+  console.log(Object.keys(games));
   /*creates variable 'render games'
    *maps through games
    *assigns a link to each game title
    *sets a new page extension equal to the gameID*/
-  const renderGames = Object.keys(games).map((gameID) => (
-    <div key={gameID} id="game-library">
-      <Link to={`/games/${gameID}`} id="game-title">
-          {games[gameID].title}   
-      </Link>
+  const renderGames = games.map((gameObject) => (
+    <div key={gameObject.id} id="game-library">
+       <Link to={`/games/${gameObject.id}`} id="game-title">
+           {gameObject.title}   
+       </Link>
     </div>
   ));
   //returns render games wrapped in div element  
